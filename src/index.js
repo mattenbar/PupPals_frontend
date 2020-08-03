64c7afa1ef792cf4cb6d2e86d6a3c619c6c9af07
mattenbar <matthw.enbar@gmail.com>
2020-08-03 18:45:49 -0400

    photo uploading now working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // main box where i want to display data
 const containerBox = document.getElementById('container-box')
 const usersUrl = "http://localhost:3000/api/v1/users/"
-
+const loginUrl = "http://localhost:3000/api/v1/login"
+let userDataEdit = {}
 
 // set switch statement to start on login page
-let state = {page: "login" }
+let state = {page: "login", user: null }
 
 // html for login page
 let loginPage = `
@@ -89,24 +90,23 @@ let registerPage =`
 // html for user profile
 function profilePage(){
   displayCard = document.getElementById("display-card")
-
   displayCard.innerHTML +=`
   <div class="profile-card" id="profile-card">
     <br>
-    <h1> ${capitalize(user.owner)}'s Profile </h1>
-    <img class="card-img-top" src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/116266925_152033579835041_6801224975812712869_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=105&_nc_ohc=8su9FYI7zr8AX-EjwNO&oh=245a0859f445c893326b39d2be1480bf&oe=5F4BFCEA">
+    <h1> Your Profile </h1>
+    <img class="card-img-top" src="${state.user.img}">
     <div class="card-body">
-      <h5 class="card-title">Pup's Name: ${capitalize(user.pet_name)}</h5>
-      <p class="card-text">${user.about}</p>
+      <h5 class="card-title">Pup's Name: ${capitalize(state.user.pet_name)}</h5>
+      <p class="card-text">${state.user.about}</p>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item rounded">Breed: ${capitalize(user.breed)}</li>
-      <li class="list-group-item rounded">Age: ${user.age}</li>
-      <li class="list-group-item rounded">Size: ${capitalize(user.size)}</li>
-      <li class="list-group-item rounded">Gender: ${capitalize(user.sex)}</li>
+      <li class="list-group-item rounded">Breed: ${capitalize(state.user.breed)}</li>
+      <li class="list-group-item rounded">Age: ${state.user.age}</li>
+      <li class="list-group-item rounded">Size: ${capitalize(state.user.size)}</li>
+      <li class="list-group-item rounded">Gender: ${capitalize(state.user.sex)}</li>
     </ul>
     <div class="card-body">
-      <a href="#" class="card-link">Edit Profile</a>
+      <a href="#" class="card-link" id="edit-link">Edit Profile</a>
     </div>
   </div>
   <br>
@@ -115,7 +115,6 @@ function profilePage(){
   </div>
 `
 }
-
 
 //capitalize function
 function capitalize(string){
@@ -151,6 +150,8 @@ function render(){
       profileButton = document.getElementById('profile-button')
       searchButton = document.getElementById('search-button')
       matchButton = document.getElementById('match-button')
+      //edit button
+      editButton = document.getElementById("edit-link")
       //event listeners for menu buttons
       searchButton.addEventListener("click", function(){
         state.page = "search"
@@ -160,19 +161,31 @@ function render(){
         state.page = "profile"
         render()
       })
-
+      editButton.addEventListener("click", function(e){
+        e.preventDefault
+        state.page = 'edit'
+        render()
+      })
       
       // logout button and event listener for the button
       logoutButton = document.getElementById('logout')
       logoutButton.addEventListener("click", function (){
         sessionStorage.clear()
         state.page = "login" 
+        state.user = null
         render()
       })
     break;
     // search page
     case 'search':
       fetchRandomUser()
+    break;
+    case 'edit':
+      editpage()
+      const saveButton = document.getElementById("saveEdit")
+      saveButton.addEventListener("click", (e) => editFormHandler(e))
+      const imgUploadButton = document.getElementById("imgUploadButton")
+      imgUploadButton.addEventListener("click", (e) => imgUploadHandler(e))
     break;
   }
 }
@@ -184,7 +197,7 @@ if (!!parseInt(sessionStorage.currentUserId)){
   .then(response => response.json())
   .then(json => {
     state.page = "profile"
-    user = json 
+    state.user = json 
     render()
   });
   } else{
@@ -207,7 +220,7 @@ function loginFetch(email, password){
         password: password
       }
   }
-  fetch("http://localhost:3000/api/v1/login", {
+  fetch(loginUrl, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify(bodyData)
@@ -219,12 +232,13 @@ function loginFetch(email, password){
       sessionStorage.currentUserId = json.id
       console.log(json)
       state.page = "profile"
-      user = json 
+      state.user = json 
       render()
       logoutButton = document.getElementById('logout')
       logoutButton.addEventListener("click", function (){
         sessionStorage.clear()
         state.page = "login" 
+        state.user = null
         render()
       })
     }
@@ -233,7 +247,6 @@ function loginFetch(email, password){
 
 // creating new users
 //listens for register button to be clicked to render register form
-
 function registerFormAdder(e) {
     e.preventDefault()
   state.page = 'register'
@@ -276,17 +289,11 @@ function registerFetch(email, password, owner, petName, age, about, breed, size,
     body: JSON.stringify(userData)})
   .then(response => response.json())
   .then(json => {
+    sessionStorage.clear()
     if (json.id){
       sessionStorage.currentUserId = json.id
       state.page = "profile"
       render()
-      logoutButton = document.getElementById('logout')
-      logoutButton.addEventListener("click", function (){
-        sessionStorage.clear()
-        state.page = "login" 
-        render()
-      })
-      return user = json 
     }
   })
 }
@@ -309,7 +316,7 @@ function fetchRandomUser(){
     profileCard.innerHTML =`
       <br>
       <h1> ${capitalize(randomUser.owner)} </h1>
-      <img class="card-img-top" src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/116266925_152033579835041_6801224975812712869_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=105&_nc_ohc=8su9FYI7zr8AX-EjwNO&oh=245a0859f445c893326b39d2be1480bf&oe=5F4BFCEA">
+      <img class="card-img-top" src="${randomUser.img}">
       <div class="card-body">
         <h5 class="card-title">Pup's Name: ${capitalize(randomUser.pet_name)}</h5>
         <p class="card-text">${randomUser.about}</p>
@@ -323,7 +330,7 @@ function fetchRandomUser(){
     `
   })
 }
-
+//top menu bar
 function menuBar(){
   containerBox.innerHTML = `
   <br>
@@ -344,4 +351,111 @@ function menuBar(){
   <div class="display-card" id="display-card">
   </div>
   `
+}
+// edit page
+function editpage(){
+  displayCard.innerHTML=`
+  <br>
+  <h3 class="text-center text-info">Edit</h3>
+  <br>
+  <img class="card-img-top" src="${state.user.img}">
+  <br><br>
+  <form id="edit-form" class="form" action="">
+  <label for="img">Select image:</label>
+  <input type="file" id="img" name="img" accept="image/*" >
+  <br><br>
+  <input type="submit" id="imgUploadButton" class="btn btn-info btn-md">
+  </form>
+  <br><br>
+  <form id="edit-form" class="form" action="" method="PUT">
+    <div class="form-group">
+      <label for="owner" class="text-info">Owner Name:</label><br>
+      <input type="text" name="owner" id="owner" class="form-control" value="${state.user.owner}" >
+    </div>
+    <div class="form-group">
+      <label for="pet_name" class="text-info">Pet Name:</label><br>
+      <input type="text" name="pet_name" id="pet_name" class="form-control" value="${state.user.pet_name}">
+    </div>
+    <div class="form-group">
+      <label for="age" class="text-info">Pet Age:</label><br>
+      <input type="number" name="age" id="age" class="form-control" min="0" max="30" value="${state.user.age}">
+    </div>
+    <div class="form-group">
+      <label for="abour" class="text-info">About Me/ Pet:</label><br>
+      <textarea name="about" class="form-control" id="about" rows="3">${state.user.about}</textarea>
+    </div>
+    <div class="form-group">
+      <label for="breed" class="text-info">Breed:</label><br>
+      <input type="text" name="breed" id="breed" class="form-control" value="${state.user.breed}">
+    </div>
+    <div class="form-group">
+      <label for="size">Select Size</label>
+      <select class="form-control" id="size">
+        <option>Small</option>
+        <option>Medium</option>
+        <option>Large</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="sex">Select Gender</label>
+      <select class="form-control" id="sex">
+        <option>Female</option>
+        <option>Male</option>
+      </select>
+    </div>
+    <div class="form-group text-left">
+      <input id="saveEdit" type="submit" name="edit" class="btn btn-info btn-md" value="Save">
+    </div>
+  </form>`
+}
+// edit form hangler
+function editFormHandler(e){
+  e.preventDefault()
+  const ownerInputEdit = document.querySelector("#owner").value
+  const petNameInputEdit = document.querySelector("#pet_name").value
+  const ageInputEdit = document.querySelector("#age").value
+  const aboutInputEdit = document.querySelector("#about").value
+  const breedInputEdit = document.querySelector("#breed").value
+  const sizeInputEdit = document.querySelector("#size").value
+  const sexInputEdit = document.querySelector("#sex").value
+  editFetch(ownerInputEdit, petNameInputEdit, ageInputEdit, aboutInputEdit, breedInputEdit, sizeInputEdit, sexInputEdit)
+}
+function editFetch(owner, petName, age, about, breed, size, sex){
+  userDataEdit= {
+    id: state.user.id,
+    owner: owner,
+    pet_name: petName,
+    about: about,
+    breed: breed,
+    size: size,
+    sex: sex,
+    age: age
+  }
+
+  fetch(("http://localhost:3000/api/v1/users/" + state.user.id),{
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(userDataEdit)
+  })
+  .then(resp => resp.json())
+  .then(json => {
+    state.user = userDataEdit
+    state.page = "profile"
+    render();
+  })
+}
+
+function imgUploadHandler(e){
+  e.preventDefault()
+  const body = new FormData()
+  const newImg = document.querySelector("#img").files[0]
+  body.append('file', newImg)
+  body.append('user_id', state.user.id)
+  fetch("http://localhost:3000/api/v1/img-upload",{
+    method: "PUT",
+    body
+  } )
 }
